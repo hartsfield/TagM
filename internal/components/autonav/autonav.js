72e0b383d473901a57aaa5f47550ef0540ca340a
John Hartsfield <john@unilink.local>
2025-02-17 22:17:23 -0500

    updates

--- internal/components/autonav/autonav.js
+++ internal/components/autonav/autonav.js
@@ -1,13 +1,3 @@
-//let np = document.getElementById("nav-portrait");
-//np.style.position = "absolute";
-//np.style.right = "-" + np.offsetWidth + "px";
-//function showNavPortrait() {
-//        np.style.right = 0;
-//        setTimeout(function () {
-//                document.addEventListener('click', tf, false);
-//        }, 50);
-//}
-
 var authToggled = false;
 
 var newToggled = false;
