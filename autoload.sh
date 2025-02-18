98153f80c1b2fc122e55eaa025df5987671c0bd3
John Hartsfield <john@unilink.local>
2025-02-17 22:16:03 -0500

    updates

--- autoload.sh
+++ autoload.sh
@@ -2,8 +2,8 @@
 # :autocmd BufWritePost * silent! !./autoload.sh
 #!/bin/bash
 HMACSS="eiwojvioejwoivn_testing_oiewnv4f2332f32fedwe2"
-pkill multiparty || true
-go build -o multiparty >>log.txt 
-./multiparty >>log.txt 2>&1 &
+pkill tagmachine.xyz || true
+go build -o tagmachine.xyz >>log.txt 
+./tagmachine.xyz >>log.txt 2>&1 &
 echo http://localhost:15111
 echo $(date +%s) > .lastsavetime_bolt
